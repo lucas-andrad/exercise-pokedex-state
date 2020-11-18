45bfdeb081ee5a76c80966e70627be790123ed87
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-18 20:24:07 -0300

    Implementando função mudar pokemon

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,37 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.changePokemon = this.changePokemon.bind(this);
+        this.state = {
+            Pokemons: props.pokemons,
+            actualPokemon: props.pokemons[0],
+            numberState: 0,
+        }
+    }
+    changePokemon() {
+        const { Pokemons, numberState } = this.state;
+        if (numberState >= (Pokemons.length - 1)) {
+            this.setState({
+                Pokemons: this.props.pokemons,
+                actualPokemon: this.props.pokemons[0],
+                numberState: 0,
+            })
+        } else{
+            this.setState({
+                actualPokemon: Pokemons[numberState + 1],
+                numberState: numberState + 1
+        })  
+        }
+
+    }
     render() {
+        const { actualPokemon } = this.state;
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                {<Pokemon pokemon={actualPokemon} />}
+                <button onClick={this.changePokemon}>Próximo Pokemon</button>
             </div>
         );
     }
